59ce6d0a91da88cb6e81e5803c2d3d7dbed3d410
Ahmed K Madani <ahmedkamal543@gmail.com>
2022-03-21 00:53:50 +0300

    create custom middleware

--- example.go
+++ example.go
@@ -102,6 +102,14 @@ func main() {
 	gin.ForceConsoleColor()
 	// r := gin.Default()
 	r := gin.New()
+	r.Use(Logger())
+
+	r.GET("/test", func(c *gin.Context) {
+		example := c.MustGet("example").(string)
+
+		//it would print: "12345"
+		log.Println(example)
+	})
 
 	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 
@@ -200,3 +208,25 @@ func main() {
 // 	}
 // 	return t, nil
 // }
+
+//Create Custom Middelware
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		//set example variable
+		c.Set("example", "12345")
+
+		// before request
+		c.Next()
+
+		// after request
+		latency := time.Since(t)
+		log.Print(latency)
+
+		//access the status we are sending
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
